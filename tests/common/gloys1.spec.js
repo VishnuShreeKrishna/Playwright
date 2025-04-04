8d6175b484ba371558274b5ff6fab307a3e6919a
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2025-04-04 09:43:39 +0530

    commit

--- tests/common/gloys1.spec.js
+++ tests/common/gloys1.spec.js
@@ -2,8 +2,16 @@ import {test, expect } from "@playwright/test";
 
 test("test", async ({page}) => {
     await page.goto("http://178.16.143.20:5000/");
-    await page.pause();
-    
+    // await page.pause();
+    await page.waitForLoadState('networkidle');
+    await page.getByRole('link',{name: 'Login'}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator('[id="username"]').fill("vishnu.sharma@itdoseinfo.com");
+    await page.locator('[id="password"]').fill("Vishnu@123");
+    await page.getByRole('button',{name:'Sign In'}).click();
+    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(5000);
+
 
 
 })
